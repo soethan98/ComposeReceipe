5a377c9654e4f1d6e00b11ff11b4993e68116872
soethan98 <soethan762@gmail.com>
2021-03-20 23:22:51 +0630

    Fragment interoperability

--- app/src/main/java/com/example/composerecipeapp/presentation/MainActivity.kt
+++ app/src/main/java/com/example/composerecipeapp/presentation/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
@@ -26,9 +28,13 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            Meal()
-        }
+        setContentView(R.layout.activity_main)
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_container, RecipeListFragment())
+            .commit()
+//        setContent {
+//            Meal()
+//        }
 
     }
 }
@@ -41,12 +47,27 @@ fun Meal() {
             contentDescription = "Image", modifier = Modifier.height(300.dp),
             contentScale = ContentScale.Crop
         )
+
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(
-                text = "Happy Meal", style = TextStyle(
-                    fontSize = 26.sp
+            Row(
+                horizontalArrangement = Arrangement.SpaceBetween,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(
+                    text = "Happy Meal", style = TextStyle(
+                        fontSize = 26.sp
+                    )
                 )
-            )
+                Text(
+                    text = "$5.99",
+                    style = TextStyle(
+                        color = Color(0xFF85bb65),
+                        fontSize = 17.sp
+                    ),
+                    modifier = Modifier.align(Alignment.CenterVertically)
+                )
+
+            }
             Spacer(modifier = Modifier.padding(top = 8.dp))
             Text(
                 text = "800 calories",
@@ -55,13 +76,14 @@ fun Meal() {
                 )
             )
             Spacer(modifier = Modifier.padding(top = 8.dp))
-            Text(
-                text = "$5.99",
-                style = TextStyle(
-                    color = Color(0xFF85bb65),
-                    fontSize = 17.sp
-                )
-            )
+
+            Button(
+                onClick = { },
+                modifier = Modifier.align(Alignment.CenterHorizontally)
+            ) {
+                Text(text = "ORDER NOW")
+            }
+
 
         }
     }
